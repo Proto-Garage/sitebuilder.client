7bb4143b6e83084fe071f4dfccd57b028ed621bd
Noah John Ucab <noahjohn.ucab@gmail.com>
2018-11-26 18:14:58 +0800

    fix(*): expose site attr

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,6 +1,6 @@
 import 'whatwg-fetch';
 import Config, { SiteAttributes, Node, SerializedNode, RootNodeAttributes, RootNode, PageNodeAttributes, PageNode, HeaderNodeAttributes, HeaderNode, FooterNodeAttributes, FooterNode, SectionNodeAttributes, SectionNode, ImageNode } from './config';
-export { Node, Config, SerializedNode, RootNodeAttributes, RootNode, PageNodeAttributes, PageNode, HeaderNodeAttributes, HeaderNode, FooterNodeAttributes, FooterNode, SectionNodeAttributes, SectionNode, ImageNode, };
+export { SiteAttributes, Node, Config, SerializedNode, RootNodeAttributes, RootNode, PageNodeAttributes, PageNode, HeaderNodeAttributes, HeaderNode, FooterNodeAttributes, FooterNode, SectionNodeAttributes, SectionNode, ImageNode, };
 export default class {
     private options;
     constructor(options: {
--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ import Config, {
 } from './config';
 
 export {
+  SiteAttributes,
   Node,
   Config,
   SerializedNode,
