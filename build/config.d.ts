266ecca68df369b669b6bf9c58674b251cd91290
noahjohn9259 <noahjohn.ucab@gmail.com>
2018-12-25 23:36:06 +0800

    0.1.63

--- build/config.d.ts
+++ build/config.d.ts
@@ -104,7 +104,7 @@ export declare type ProductAttributes = {
     description: string;
 };
 export declare type MultiProductNodeAttributes = {
-    items?: ProductAttributes[];
+    items: ProductAttributes[];
 };
 export declare type MultiProductNode = Node<'MultiProduct', MultiProductNodeAttributes>;
 export declare type GraphicPlateNodeAttributes = {
@@ -160,10 +160,6 @@ export default class Config {
         type: 'Text';
         attributes: TextNodeAttributes;
     }): TextNode;
-    createNode(params: {
-        type: 'ImageArray';
-        attributes: ImageNodeAttributes;
-    }): ImageNode;
     createNode(params: {
         type: 'Image';
         attributes: ImageNodeAttributes;
